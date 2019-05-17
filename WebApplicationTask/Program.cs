179f773daede83a6352aa2d7b1d8bd0dc44c1d95
Luiz <luizfabianogarrett@gmail.com>
2019-05-16 21:42:07 -0300

    up

---
+++ WebApplicationTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using Hangfire;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplicationTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //GlobalConfiguration.Configuration.UseMemoryStorage();
+    
+            BuildWebHost(args).Run();
+          
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ WebApplicationTask/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Hangfire;
+using Hangfire.Common;
+using Hangfire.Dashboard;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace WebApplicationTask
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public static int Autenticado { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            
+            //services.AddHangfire(x => x.UseSqlServerStorage("<connection string>"));
+            var inMemory = GlobalConfiguration.Configuration.UseMemoryStorage();
+            services.AddHangfire(x => x.UseStorage(inMemory));
+            JobStorage.Current = inMemory;
+
+
+            services.AddMvc();
+
+            RecurringJob.AddOrUpdate("Job Write por 1 minuto", () => Console.WriteLine("Recurring 1 Minuto!" + DateTime.Now), Cron.MinuteInterval(1));
+            RecurringJob.AddOrUpdate("Job Write por 2 minuto", () => Console.WriteLine("Recurring 2 Minutos!" + DateTime.Now), Cron.MinuteInterval(2));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseAuthentication();
+
+            app.UseHangfireServer();
+           
+            app.UseMvc();
+
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new HFDashboardAuthFilter() }
+            });
+
+           
+
+        }
+
+
+    }
+}
+
